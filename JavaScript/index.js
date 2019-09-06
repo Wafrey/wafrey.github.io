b6ed2b0b4e2d2249ec108ea006adec2846f32796
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-09-06 15:06:06 +0300

    Add preloader

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+(function () {
+
+    let preload = document.getElementById('preload');
+    let loading = 0;
+    let id = setInterval(frame, 64);
+
+    function frame() {
+        if (loading === 70) {
+            clearInterval(id);
+            window.open('main.html', '_self');
+        } else {
+            loading += 1;
+            if (loading==60) {
+                preload.style.animation='fadeout 1s ease'
+            }
+        }
+    }
+})();
